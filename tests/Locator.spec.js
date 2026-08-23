40eeedd77e2e1bd028e322d7438c141edb6dde54
soumikroyhpe <soumik.royhpe@gmail.com>
2026-08-23 20:57:19 +0100

    Day2_locator_1

--- tests/Locator.spec.js
+++ tests/Locator.spec.js
@@ -1,1 +1,22 @@
 import {test, expect} from '@playwright/test';
+
+test('Locators', async({page})=>{
+
+   await page.goto('https://www.saucedemo.com/');
+
+   await page.locator('#user-name').fill('standard_user')
+
+   await page.locator('#password').fill('secret_sauce')
+
+   await page.locator('.submit-button').click()
+   
+  // page.waitForLoadState
+
+   await expect(page).toHaveURL(/inventory.html/); //verify url
+   const productElement = page.locator('.title')
+   await expect(productElement).toHaveText('Products') // we can use "toContainText" here
+
+   await expect(productElement).toBeVisible()// verify web element visibility 
+   await page.pause()
+
+})
